2440521fe26e8ecd2d71c1fea25972f16cfa5f2e
amjadsh92 <amjadsharafeddien@gmail.com>
2024-07-03 09:21:03 +0200

    changing th title of the web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+  title: "MyProfile",
   viewport: "width=device-width, initial-scale=1.0",
 };
 
